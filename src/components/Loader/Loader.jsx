362ffb35c6089b7f029b010a7ed9f53bc219c796
PlatinumKellerman <platinumm@ukr.net>
2022-07-10 23:05:25 +0300

    fix

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,6 +1,11 @@
 import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 import { ThreeDots } from 'react-loader-spinner';
+import { ButtonWrapper } from './Loader.styled';
 
 export const Loader = () => {
-  return <ThreeDots color="tomato" height={100} width={100} />;
+  return (
+    <ButtonWrapper>
+      <ThreeDots color="tomato" height={100} width={100} />;
+    </ButtonWrapper>
+  );
 };

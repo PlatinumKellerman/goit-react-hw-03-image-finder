72136f4b4b20dce1c3bb16957e04dedcbb73d0a9
PlatinumKellerman <platinumm@ukr.net>
2022-07-10 19:45:11 +0300

    in-progress

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,21 +1,38 @@
 import { ImageGalleryItem } from '../ImageGalleryItem/index';
 import { GalleryList } from './ImageGallery.styled';
 import { LoadMoreButton } from '../LoadMoreButton/index';
-
+import { Modal } from '../Modal/index';
 import { Component } from 'react';
 
 export class ImageGallery extends Component {
+  state = {
+    isOpen: false,
+    largeImageURL: '',
+  };
+
+  onModalOpen = () => {
+    this.setState({ isOpen: true });
+  };
+
+  onModalClose = () => {
+    this.setState({ isOpen: false });
+  };
+
   render() {
     const NumberOfPics = this.props.pics.length;
     const totalHits = this.props.totalHits;
     return (
       <>
         <GalleryList>
-          <ImageGalleryItem pics={this.props.pics} />
+          <ImageGalleryItem
+            pics={this.props.pics}
+            onModalOpen={this.onModalOpen}
+          />
         </GalleryList>
         {NumberOfPics >= 12 && NumberOfPics < totalHits && (
           <LoadMoreButton loadMore={this.props.loadMore} />
         )}
+        {this.state.isOpen && <Modal onClose={this.onModalClose} />}
       </>
     );
   }
